5a0ffafdf4e2882b1f500f79bfb324d082311652
alt-browne <aidanlouietempleton-browne@hotmail.com>
2023-02-21 20:52:48 +1100

    Tidy code for highlighting winning squares

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function Board({ xIsNext, squares, onPlay }) {
                 for (let i = 0; i < 3; i++) {
                         let row = [];
                         for (let j = 0; j < 3; j++) {
-                                row.push(<Square key={i * 3 + j} value={squares[i * 3 + j]} winningSquare={winner[0] && winner[1].some(rowCol => rowCol[0] === i && rowCol[1] === j) ? true : false} onSquareClick={() => handleClick(i * 3 + j)} />);
+                                row.push(<Square key={i * 3 + j} value={squares[i * 3 + j]} winningSquare={winner[0] && winner[1].some(arrIndex => Math.floor(arrIndex / 3) === i && arrIndex % 3 === j) ? true : false} onSquareClick={() => handleClick(i * 3 + j)} />);
                         }
                         board.push(
                                 <div key={i} className="board-row">
@@ -104,19 +104,19 @@ export default function Game() {
 
 function calculateWinner(squares) {
         const lines = [
-                [[0, 1, 2], [[0, 0], [0, 1], [0, 2]]],
-                [[3, 4, 5], [[1, 0], [1, 1], [1, 2]]],
-                [[6, 7, 8], [[2, 0], [2, 1], [2, 2]]],
-                [[0, 3, 6], [[0, 0], [1, 0], [2, 0]]],
-                [[1, 4, 7], [[0, 1], [1, 1], [2, 1]]],
-                [[2, 5, 8], [[0, 2], [1, 2], [2, 2]]],
-                [[0, 4, 8], [[0, 0], [1, 1], [2, 2]]],
-                [[2, 4, 6], [[0, 2], [1, 1], [2, 0]]]
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
         ];
         for (let i = 0; i < lines.length; i++) {
-                const [a, b, c] = lines[i][0];
+                const [a, b, c] = lines[i];
                 if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                        return [squares[a], lines[i][1]];
+                        return [squares[a], lines[i]];
                 }
         }
         return [null, null];
